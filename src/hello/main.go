668bc4ed87f66bfc01a39984b763bf7b81639c7f
KoutaHisano <hisano.k@arsaga.jp>
2023-03-05 22:05:55 +0900

    practice

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
-func main() {
-    fmt.Printf("hello, world\n")
+func main()  {
+    animal := [...]string{"dog", "cat", "parrot"}
+    fmt.Println(animal[0])
+    fmt.Println(animal[1])
+    fmt.Println(animal[2])
+
+    x := 10
+    y := 2
+
+    x++
+    y--
+
+    fmt.Println(x)
+    fmt.Println(y)
 }
